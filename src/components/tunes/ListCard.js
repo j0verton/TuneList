7ab0c20c62e3075920319fe2989713a5a03eb6a5
j0verton <overtonje@gmail.com>
2020-10-20 13:52:19 -0500

    stuck on this modal logic, about to do a big refactor

--- src/components/tunes/ListCard.js
+++ src/components/tunes/ListCard.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useContext, useEffect, useState, useRef } from "react"
 import "./Tune.css"
 import { Button, Container, Divider, Header, Icon } from 'semantic-ui-react'
 import { TuneContext } from "./TuneProvider"
@@ -8,20 +8,29 @@ import { TuneCard } from "./TuneCard"
 export const ListCard = ({tunesArr}) => {
     const [ modal, showModal ] = useState(false)
     const [ tuneObj,setTuneObj] = useState({})
+    const tuneRef= useRef(null)
 
+//this function is 
     function findTuneInArray(id) {
+        console.log("id passed to find", id)
+        console.log("tuneobj",tuneObj)
         return tunesArr.find(arrTune=>id===arrTune.id)
     }
+
+
     return tunesArr[0] ? (
         <>
         {console.log("tunesArr", tunesArr)}
         {tunesArr.map(tune => {
             console.log("tune in list card",tune)
             return <><Button as='h3' 
+            ref={tuneRef}
             onClick={e=>{
                 console.log("e",e)
                 showModal(true)
-                setTuneObj(findTuneInArray(e.target.id))
+                setTuneObj(()=>{
+                    // console.log("ref?",tuneRef.current.props.id)
+                    findTuneInArray(tuneRef.current.props.id)})
             }} 
                 id={tune.id} 
                 className="tuneEntry">
