4d28a20296cde3db65818fa71485ba6cc2fe9e31
j0verton <overtonje@gmail.com>
2020-10-20 14:03:56 -0500

    moved modal into tuneCard still triggering both modals

--- src/components/tunes/ListCard.js
+++ src/components/tunes/ListCard.js
@@ -1,46 +1,57 @@
 import React, { useContext, useEffect, useState, useRef } from "react"
 import "./Tune.css"
-import { Button, Container, Divider, Header, Icon } from 'semantic-ui-react'
+import { Button, Container, Divider, Header, Icon, Modal } from 'semantic-ui-react'
 import { TuneContext } from "./TuneProvider"
 import { Link, useHistory } from "react-router-dom"
-import { TuneCard } from "./TuneCard"
+
 
 export const ListCard = ({tunesArr}) => {
-    const [ modal, showModal ] = useState(false)
-    const [ tuneObj,setTuneObj] = useState({})
-    const tuneRef= useRef(null)
-
-//this function is 
-    function findTuneInArray(id) {
-        console.log("id passed to find", id)
-        console.log("tuneobj",tuneObj)
-        return tunesArr.find(arrTune=>id===arrTune.id)
-    }
-
+    const [open, setOpen] = useState(false)
+    const { deleteTune } = useContext(TuneContext)
+    const history = useHistory()
 
     return tunesArr[0] ? (
         <>
         {console.log("tunesArr", tunesArr)}
         {tunesArr.map(tune => {
             console.log("tune in list card",tune)
-            return <><Button as='h3' 
-            ref={tuneRef}
-            onClick={e=>{
-                console.log("e",e)
-                showModal(true)
-                setTuneObj(()=>{
-                    // console.log("ref?",tuneRef.current.props.id)
-                    findTuneInArray(tuneRef.current.props.id)})
-            }} 
-                id={tune.id} 
-                className="tuneEntry">
-                {tune.name}
-                </Button>
+            return <>
+            <Modal
+                closeIcon
+                size="mini"
+                trigger={
+                    <Button as='h3'                
+                        id={tune.id} 
+                        className="tuneEntry"
+                    >
+                        {tune.name}
+                    </Button>}
+                open={open}
+                onClose={() => setOpen(false)}
+                onOpen={() => setOpen(true)}
+                className="tune--container"
+                >
+                <Modal.Content className="tune--headercontainer">
+                <Header as='h3'>{tune.name}</Header>
+                <Header as='h4'>{tune.key/tune.tuning}</Header>
+                </Modal.Content>
+                <p>source: {tune.source}</p>
+                <p>{tune.notes}</p>
+                <a href={tune.link}>listen</a>
+                <Modal.Actions>
+                    <Button icon onClick={() => {
+                        history.push(`/tunes/edit/${tune.id}`)
+                    }}><Icon name='edit outline' /></Button>
+                    <Button color="red" icon id="deleteTune--${tune.id}" className="trashBtn" onClick={
+                        () => {
+                            deleteTune(tune.id)
+                        }}><Icon name='trash alternate outline' /></Button>
+                </Modal.Actions>
+            </Modal>
                 </>
             
             })
     }
-    {modal ? <TuneCard id={tuneObj.id} tuneObj={tuneObj} closeModal={()=>showModal(false)}/> : null }
     </>
     
     ) : null
