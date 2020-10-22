8bdc05205f70055a7b992beb983f9645d3ad7870
j0verton <overtonje@gmail.com>
2020-10-22 13:18:26 -0500

    styling

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Container, Divider, Segment, Header, Icon } from "semantic-ui-react"
+import { Container, Divider, Segment, Header, Icon, Image } from "semantic-ui-react"
 import { TuneContext } from "./components/tunes/TuneProvider";
-import logo from "./img/"
+import logo from "./img/TuneListLogo.png"
 import "./Home.css"
 
 export const Home = () => {
@@ -17,12 +17,10 @@ export const Home = () => {
 
     return (
         <>
+            {/* <Header><Image src={logo} floated="left" size="medium" alt="TuneList logo, a fiddle over 3 sheets of paper" className="LogoHome" />
+            </Header> */}
           <div className="homeContainer">
             <div className="homeInfo">
-              <img src={logo} alt="IBS logo" className="ibsLogoHome" />
-                welcome to TuneList
-           
-
             </div>
             <Divider horizontal>
               <Header as='h4'>
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,17 +1,17 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import { Icon, Image } from "semantic-ui-react"
-// import logo from "../../img/ibs-logo.png"
+import logo from "../../img/TuneListLogo.png"
 import "./NavBar.css"
 
 export const NavBar = (props) => {
     return (
         <section className="navbar__container">
+            
             <ul className="navbar">
-                <li className="navbar__item active">
+                <li className="navbar__item navbar__logo active">
                     <Link className="navbar__link" to="/">
-                        Home
-                        {/* <Image src={logo} alt="IBS logo" className="navbar__logo" /> */}
+                       <Image src={logo} floated="left" size="medium" alt="TuneList logo, a fiddle over 3 sheets of paper" className="LogoNav" />
                     </Link>
                 </li>
                 <li className="navbar__item active">
